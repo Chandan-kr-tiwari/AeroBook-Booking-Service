3864ae83282427205fcf0d0ca39dbb2ea217dbaa
Chandan Tiwari <chandan.kr.tiwarii@gmail.com>
2026-09-08 09:19:48 +0000

    added aerobook-booking-service starter

---
+++ src/config/server-config.js
@@ -0,0 +1,9 @@
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    PORT: process.env.PORT,
+    AEROBOOK_FLIGHT_SERVICE: process.env.AEROBOOK_FLIGHT_SERVICE
+}
---
+++ src/utils/common/enums.js
@@ -0,0 +1,8 @@
+const BOOKING_STATUS = {
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    INITIATED: 'initiated',
+    PENDING: 'pending'
+}
+
+module.exports = {BOOKING_STATUS}
